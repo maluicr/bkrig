34b87569725dce6479c86ce603c3e01b10135ed4
Manuel Ribeiro <mribeiro@cmrp.ist.utl.pt>
2020-08-20 19:18:01 +0100

    creates cases file for dss
    added block of code to generate .out file with cases data. added day of cases to argument of function. returns .out file in 'input' folder.

--- f_irates.R
+++ f_irates.R
@@ -1,6 +1,6 @@
 
-irates = function(df = NA, fid = NA, fx = NA, fy = NA, fz = NA, fcases = NA, fpop = NA){
-  
+irates = function(df = NA, fid = NA, fx = NA, fy = NA, fz = NA, fcases = NA, fpop = NA, fday = "20200301"){
+
   # rate per phab habitants
   phab = 10^4 
   
@@ -26,11 +26,56 @@ irates = function(df = NA, fid = NA, fx = NA, fy = NA, fz = NA, fcases = NA, fpo
   error = m / p
   
   # NA cases set to 2 cases
+  df[, fcases] = ifelse(is.na(df[, fcases]), 2, df[, fcases])
   c = df[, fcases]
   
   # recalculate crude rates
   rate = phab * c / p
   
-  tab = data.frame (id, cx, cy, cz, rate, error)
-  return(tab)
+  tab = data.frame (id, x = cx, y = cy, z = cz, rate, error)
+  
+  # cases file for dss
+  
+  # set folder 
+  foldin = "input"
+  
+  # create folder for inputs
+  if(!file.exists(foldin)) dir.create(foldin, recursive=T)  
+  
+  # store string with path for input files
+  wkin = paste0(getwd(), "/", foldin)
+  
+  # prepare data to write file  
+  tabnotf = tab[, c("x", "y", "z", "rate")]
+  
+  # store nr of variables
+  nvars = ncol(tabnotf)
+  
+  # store nr of observations
+  nobs = nrow(tabnotf)
+  
+  # store vector variable names
+  namevars = names(tabnotf)
+
+  # create file path
+  not_name = "notified"
+  not_nameO = paste0(not_name, ".out")
+  fnot = paste0(wkin, "/", fday, not_nameO)
+  
+  if (file.exists(fnot)){
+    file.remove(fnot)  
+  }
+  
+  # create notification file
+  file.create(fnot)
+  
+  # write file header
+  cat(not_name, file = fnot, sep="\n")
+  cat(nvars, file = fnot, sep="\n", append = T)
+  cat(namevars, file = fnot, sep="\n", append = T)
+  
+  # write notification data
+  return(write.table(format(tabnotf, digits = NULL, justify = "right"),
+              file = fnot, quote = F, append = T, row.names = F, col.names = F))
+
 }
