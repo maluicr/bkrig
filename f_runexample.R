2a0e6f9b87292a6d789740568cf3325896b2eef3
Manuel Ribeiro <mribeiro@cmrp.ist.utl.pt>
2020-08-21 16:49:17 +0100

    script with example
    works as a vignette. it is a script with a full example to help users running the code.

---
+++ f_runexample.R
@@ -0,0 +1,16 @@
+
+# compute rates and error variance
+tx = irates(df = "dados.txt", fid = "objectid", fx = "x", fy = "y", fz = "z", fcases = "casos", fpop = "popres18", fday = "20200302")
+
+# compute experimental variogram
+ve = varexp(tx, lag = 7000, nlags = 15)
+
+# compute theoretical variogram
+vm = varmodel(ve, mod = "Exp", nug = 0, ran = 50000, sill = ve[[1]])
+
+# plot experimental variogram
+plot(ve[[2]], ylab = expression(paste(gamma, "(h)")), xlab = "h (in m)") 
+# add sill
+abline(h = ve[[1]], col ="red", lty = 2)
+# add theoretical model
+lines(vm[[3]]) 
