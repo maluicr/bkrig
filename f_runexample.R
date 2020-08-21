238d6202954fe037b5a4491280fccfb9dfecd569
Manuel Ribeiro <mribeiro@cmrp.ist.utl.pt>
2020-08-21 20:14:38 +0100

    added blockfile function to script
    added blockfile function to the script.

--- f_runexample.R
+++ f_runexample.R
@@ -2,11 +2,14 @@
 # compute rates and error variance
 tx = irates(df = "dados.txt", fid = "objectid", fx = "x", fy = "y", fz = "z", fcases = "casos", fpop = "popres18", fday = "20200302")
 
+# create block and mask file
+grids = blockfile(tx, "gis/grid2k.tif")
+
 # compute experimental variogram
 ve = varexp(tx, lag = 7000, nlags = 15)
 
 # compute theoretical variogram
-vm = varmodel(ve, mod = "Exp", nug = 0, ran = 50000, sill = ve[[1]])
+vm = varmodel(ve, mod = "Sph", nug = 0, ran = 50000, sill = ve[[1]])
 
 # plot experimental variogram
 plot(ve[[2]], ylab = expression(paste(gamma, "(h)")), xlab = "h (in m)") 
@@ -14,3 +17,4 @@ plot(ve[[2]], ylab = expression(paste(gamma, "(h)")), xlab = "h (in m)")
 abline(h = ve[[1]], col ="red", lty = 2)
 # add theoretical model
 lines(vm[[3]]) 
+
