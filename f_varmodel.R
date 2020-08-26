7b89659fd4406ace6c5d2ad6ca7472437cd24bf2
Manuel Ribeiro <mribeiro@cmrp.ist.utl.pt>
2020-08-26 18:14:31 +0100

    corrected indexes and added objects returned
    now indexes are strings instead of numeric. added model type to returned objects (needed for ssdir.par)

--- f_varmodel.R
+++ f_varmodel.R
@@ -1,18 +1,22 @@
 
-varmodel = function (varexp, mod = c("Exp", "Sph"), nug, ran , sill) {
+varmodel = function (varexp, mod = c("Exp","Sph"), nug, ran , sill) {
+  
   x = seq(1, ran , 1)
-  xmax = seq(1, max(varexp[[2]][,"dist"]), 1)
+  xmax = seq(1, max(varexp[["semivar"]][,"dist"]), 1)
   if (mod=="Sph") {
+    vtype = 1
     model = nug + sill * (1.5 * (x / ran) - 0.5 * (x / ran)^3)
     cutoff = max(xmax) - ran
     msill = rep(nug + sill, cutoff )
     model = c(0, model, msill)
   }
   if (mod=="Exp") {
+    vtype = 2
     model = nug + sill * (1 - exp(- 3 * xmax / ran))
     model = c(0, model)
   }
-  pars = data.frame (model = mod, nugget = nug, range = ran, psill = sill)
+ 
+  pars = data.frame (model = mod, modeltype = vtype, nugget = nug, range = ran, psill = sill)
   nstruc = 1 # for now only 1 (Spherical or Exponential)
   return(list(structures = nstruc, parameters = pars, fittedval = model))
 }
