b89068b438e32a684d1960edb6a56ff1cdff9484
Manuel Ribeiro <mribeiro@cmrp.ist.utl.pt>
2020-08-26 18:25:42 +0100

    change object names and add arguments in functions

--- f_runexample.R
+++ f_runexample.R
@@ -1,29 +1,38 @@
 
 # script example to compute block kriging covid-19 (Azevedo et al, 2020)
 
+source("f_irates.R", echo = T)
+source("f_blockfile.R", echo = T)
+source("f_maskfile.R", echo = T)
+source("f_varexp.R", echo = T)
+source("f_varmodel.R", echo = T)
+source("f_ssdir.R", echo = T)
+
 # compute rates and error variance
-tx = irates(df = "dados.txt", id = "objectid", x = "x", y = "y", z = "z", 
-            cases = "casos", pop = "popres18", casesNA = 2, day = "20200302")
+covid = irates(df = "dados.txt", id = "objectid", x = "x", y = "y", z = "z", 
+            cases = "casos", pop = "popres18", casesNA = 2, day = "20200415")
 
 # create block file
-rgrid = blockfile(tx, "gis/grid2k.tif")
-plot(rgrid$ingrid)
+block = blockfile(covid, "gis/grid2k.tif")
+# plot grid input
+plot(block$ingrid)
 
 # create mask file
-m = maskfile(rgrid)
+mask = maskfile(block)
 
 # compute experimental variogram
-ve = varexp(tx, lag = 7000, nlags = 15)
+vexp = varexp(covid, lag = 7000, nlags = 15)
 
 # compute theoretical variogram
-vm = varmodel(ve, mod = "Exp", nug = 0, ran = 60000, sill = ve[[1]])
+vmod = varmodel(vexp, mod = "Exp", nug = 0, ran = 60000, sill = vexp[["weightsvar"]])
 
 # plot experimental variogram
-plot(ve[["semivar"]], ylab = expression(paste(gamma, "(h)")), xlab = "h (in m)") 
+plot(vexp[["semivar"]], ylab = expression(paste(gamma, "(h)")), xlab = "h (in m)") 
 # add sill
-abline(h = ve[["dist"]], col ="red", lty = 2)
+abline(h = vexp[["weightsvar"]], col ="red", lty = 2)
 # add theoretical model
-lines(vm[["fittedval"]]) 
+lines(vmod[["fittedval"]]) 
 
 # run ssdir.par
-ssdpars(rgrid, m, tx, vm, simulations = 2, radius1 = 60000, radius2 = 60000 )
+ssdpars(blockobj = block, maskobj = mask, dfobj = covid, varmobj = vmod, 
+        simulations = 5, radius1 = 60000, radius2 = 60000)
